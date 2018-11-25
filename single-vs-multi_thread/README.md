7da4f458d5234c7302e9e36b827aecf897669e8f
Carlos Rios <carlos@zanaca.com>
2018-11-25 20:37:54 -0200

    first test

---
+++ single-vs-multi_thread/README.md
@@ -0,0 +1,5 @@
+# Measuring
+Using `wrk` to stress te application and  collecting statistics with `kubectl`
+
+# Troubleshooting
+- if you get an error about local container not responding is because Docker has killed it because of memory
---
+++ single-vs-multi_thread/run-test.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+
+TYPE=$1
+if [ -z "$TYPE"  ]; then
+    echo "Please inform the test type. Options: single, multi E.g.: $0 single local"
+    exit 1
+fi
+
+ENVIRONMENT=$2
+if [ -z "$ENVIRONMENT"  ]; then
+    echo "Please inform the test environment. Options: local, gce E.g.: $0 single local"
+    exit 1
+fi
+
+THREADS=2
+TEST_TIME=60s
+CONNECTIONS_OPEN=100
+REQ_PER_SEC=2000
+TIMEOUT=5
+
+CPU=102
+MEMORY=60
+
+LOG_FILE="${TYPE}_${CPU}cpu_${MEMORY}Mib.log"
+
+echo "Setting up ${TYPE} test at ${ENVIRONMENT} environment with limits ${CPU}cpu/${MEMORY}MibRAM..." > ${LOG_FILE}
+cat $LOG_FILE
+
+if [[ "$ENVIRONMENT" == "local" ]]; then
+    if [[ "$TYPE" == "single" ]]; then
+        PORT=8080
+        TAG=svm-single
+        DOCKERFILE=Dockerfile_single
+    else
+        PORT=8081
+        TAG=svm-multiple
+        DOCKERFILE=Dockerfile_multi
+    fi
+    docker build -t "$TAG:latest" -f $DOCKERFILE . 1> /dev/null 2> /dev/null
+    docker run -d --memory=${MEMORY}Mib --cpu-shares=$CPU -p $PORT:8080 --name $TAG  --rm "$TAG:latest" 1> /dev/null 2> /dev/null
+    URL=http://${TAG}.hud:8080/
+else
+    touch .running
+    if [[ "$TYPE" == "single" ]]; then
+        URL=http://k8sbenchmarktestssingle.kuebe.prod.gce.hucloud.com.br/
+    else
+        URL=http://k8sbenchmarktestsmulti.kuebe.prod.gce.hucloud.com.br/
+    fi
+    TAG=$(kubectl get pods | grep k8sbenchmarktests$TYPE | cut -d\  -f1)
+fi
+
+echo "Stressing $URL with ${THREADS} threads for ${TEST_TIME} of time maintaining ${CONNECTIONS_OPEN} connections opened at ${REQ_PER_SEC}req/sec with ${TIMEOUT}s of timeout"
+
+if [[ "$ENVIRONMENT" == "local" ]]; then
+    echo -e "CPU\tMemory" > ${LOG_FILE}.csv
+    while true; do
+        docker stats $TAG --no-stream --format "{{.CPUPerc}}\t{{.MemUsage}}" >> ${LOG_FILE}.csv || break
+    done &
+else
+    echo -e "CPU" > ${LOG_FILE}.csv
+    while true; do
+        [ -f .running ] && kubectl get hpa $TAG --no-headers | cut -d\  -f7 >> ${LOG_FILE}.csv
+    done &
+fi
+
+docker run --entrypoint '' bootjp/wrk2 wrk -t$THREADS -c$CONNECTIONS_OPEN -d$TEST_TIME -R$REQ_PER_SEC --timeout $TIMEOUT --latency $URL >> ${LOG_FILE}
+
+
+if [[ "$ENVIRONMENT" == "local" ]]; then
+    echo -e "\n----------------------------------------------------------\nContainer Logs:" >> ${LOG_FILE}
+    docker logs $TAG >> ${LOG_FILE}
+else
+    rm .running
+    kubectl logs $TAG >> ${LOG_FILE}
+fi
+
+echo -e "\n----------------------------------------------------------\nContainer Stats:" >> ${LOG_FILE}
+cat ${LOG_FILE}.csv >> ${LOG_FILE}
+rm  ${LOG_FILE}.csv
+
+if [[ "$ENVIRONMENT" == "local" ]]; then
+    docker kill $TAG 1> /dev/null 2> /dev/null
+fi
